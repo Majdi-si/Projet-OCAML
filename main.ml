2f43a505db2f07d30fdc026a152ae94685cbb254
Majdi-si <majdi.si.2003@gmail.com>
2024-12-20 08:44:46 +0100

    Ajout de la fonction de tri des heures de début des vols et mise à jour de la gestion des parkings pour utiliser la liste complète des vols. Amélioration de l'affichage des informations de vol et export des retards moyens par heure dans un fichier CSV.

--- main.ml
+++ main.ml
@@ -29,9 +29,13 @@ let () =
   let heure_parking_27L = Dman.calculer_heure_parking dman_27L in
   let heure_parking_26L = Dman.calculer_heure_parking dman_26L in
   let heure_parking_27R = Dman.calculer_heure_parking dman_27R in
+  let tous_vols = heure_parking_26L @ heure_parking_26R @ heure_parking_27L @ heure_parking_27R in
   let nombre_parkings_differents = Parking.nombre_parkings_differents vols in
   let liste_parking_differents = Parking.liste_parking_differents vols in
   let hashtbl_parkings = Parking.create_hashtbl_vide nombre_parkings_differents in
-  Parking.remplir_hashtbl vols liste_parking_differents hashtbl_parkings;
+  Parking.remplir_hashtbl tous_vols liste_parking_differents hashtbl_parkings;
+  Parking.tri_heure_debut tous_vols hashtbl_parkings;
+  Vol.afficher_info tous_vols;
   Parking.afficher_hashtbl hashtbl_parkings;
-
+  Ttot.retard_moyen_par_heure heure_parking_26R;
+  Ttot.ecrire_retard_par_heure_csv heure_parking_27L "fic_15_1_26R.csv"
--- parking.ml
+++ parking.ml
@@ -54,6 +54,13 @@ let afficher_hashtbl (ht : Vol.t list ParkingHashtbl.t) : unit =
   ) ht
 
 
+let tri_heure_debut (vols : Vol.t list) (ht : Vol.t list ParkingHashtbl.t) : unit =
+  ParkingHashtbl.iter (fun parking vols ->
+    let vols_tries = List.sort (fun vol1 vol2 ->
+      compare vol1.Vol.occupation_parking.lower vol2.Vol.occupation_parking.lower
+    ) vols in
+    ParkingHashtbl.replace ht parking vols_tries
+  ) ht
 
 let calculer_intervalles_occupation (vols : Vol.t list) : unit =
   List.iter (fun (vol : Vol.t) ->
