b33e2d04e14cc97340eeb9ca40458227a671040e
Majdi-si <majdi.si.2003@gmail.com>
2024-12-23 10:22:17 +0100

    c

--- main.ml
+++ main.ml
@@ -42,5 +42,5 @@ let () =
   Parking.calculer_intervalles_occupation tous_vols;
   Parking.tri_heure_debut tous_vols hashtbl_parkings;
   Parking.info_vol_par_parking tous_vols hashtbl_parkings;
-  Parking.calcul_conflit_parking tous_vols hashtbl_parkings;
-  Traitement_donnee.ecrire_statistiques_par_heure_csv tous_vols "statistiques_par_heure.csv"
+  (*Parking.calcul_conflit_parking tous_vols hashtbl_parkings;*)
+  Traitement_donnees.ecrire_statistiques_par_heure_csv tous_vols "statistiques_par_heure.csv"
--- traitement_donnees.ml
+++ traitement_donnees.ml
@@ -1,71 +1,62 @@
-module Vol = Volmodule Vol = Vol
+module Vol = Vol
 module Parking = Parking
-module Ttot = Ttot
-
-let nb_conflits_parking_par_heure (vols : Vol.t list) =
-    let heures = Array.make 24 0 in
-    List.iter (fun (vol : Vol.t) ->
-      let heure = vol.etot / 3600 in
-      if vol.conflit_parking then
-        heures.(heure) <- heures.(heure) + 1
-    ) vols;
-    heures
-
-
-let ecrire_statistiques_par_heure_csv (vols : Vol.t list) (nom_fichier : string) =
-    let nb_vols = nb_vols_par_heure vols in
-    let nb_conflits_parking = nb_conflits_parking_par_heure vols in
-    let nb_creneaux_rates = nb_creneaux_rates_par_heure vols in
-    let heures = Array.make 24 (0, 0, 0, 0) in
-    List.iter (fun (vol : Vol.t) ->
-      let heure = vol.etot / 3600 in
-      let (total_retard, count, _, _) = heures.(heure) in
-      heures.(heure) <- (total_retard + (vol.ttot - vol.etot), count + 1, nb_conflits_parking.(heure), nb_creneaux_rates.(heure))
-    ) vols;
-    let fichier = open_out nom_fichier in
-    fprintf fichier "Heure,NB_vols,Retard moyen (minutes),Nb_conflits_parking,Nb_creneaux_rates\n";
-    Array.iteri (fun heure (total_retard, count, conflits_parking, creneaux_rates) ->
-      if count > 0 then
-        fprintf fichier "%02d,%d,%.2f,%d,%d\n" heure nb_vols.(heure) (float_of_int total_retard /. float_of_int count /. 60.0) conflits_parking creneaux_rates
-      else
-        fprintf fichier "%02d,%d,Aucun vol,%d,%d\n" heure nb_vols.(heure) conflits_parking creneaux_rates
-    ) heures;
-    close_out fichier
-
-let nb_conflits_parking_par_heure (vols : Vol.t list) =
-    let heures = Array.make 24 0 in
-    List.iter (fun (vol : Vol.t) ->
-      let heure = vol.etot / 3600 in
-      if vol.conflit_parking then
-        heures.(heure) <- heures.(heure) + 1
-    ) vols;
-    heures
 
 let nb_creneaux_rates_par_heure (vols : Vol.t list) =
-    let heures = Array.make 24 0 in
-    List.iter (fun (vol : Vol.t) ->
+  let heures = Array.make 24 0 in
+  List.iter (fun (vol : Vol.t) ->
+    Printf.printf "Vol %d %d\n" vol.etot vol.ttot;
+    if vol.ttot > vol.etot + 600 && vol.type_vol = "DEP" then 
+    let heure = vol.etot / 3600 in
+      heures.(heure) <- heures.(heure) + 1;
+      Printf.printf "Vol %s rate le créneau horaire\n" vol.indicatif
+  ) vols;
+  heures
+
+let nb_vols_par_heure (vols : Vol.t list) =
+  let heures = Array.make 24 0 in
+  List.iter (fun (vol : Vol.t) ->
+    if vol.type_vol = "DEP" then
       let heure = vol.etot / 3600 in
-      if vol.ttot > vol.etot + 600 then
-        heures.(heure) <- heures.(heure) + 1
-    ) vols;
-    heures
+      heures.(heure) <- heures.(heure) + 1
+    else if vol.type_vol = "ARR" then
+      let heure_debut = int_of_string vol.heure_debut in
+      let heure = heure_debut / 3600 in
+      heures.(heure) <- heures.(heure) + 1
+  ) vols;
+  heures
 
 let ecrire_statistiques_par_heure_csv (vols : Vol.t list) (nom_fichier : string) =
-    let nb_vols = nb_vols_par_heure vols in
-    let nb_conflits_parking = nb_conflits_parking_par_heure vols in
-    let nb_creneaux_rates = nb_creneaux_rates_par_heure vols in
-    let heures = Array.make 24 (0, 0, 0, 0) in
+  let nb_vols = nb_vols_par_heure vols in
+  let nb_creneaux_rates = nb_creneaux_rates_par_heure vols in
+  let heures = Array.make 24 (0, 0, 0, 0) in
+  List.iter (fun (vol : Vol.t) ->
+    let heure = vol.etot / 3600 in
+    let (total_retard, count, _, _) = heures.(heure) in
+    heures.(heure) <- (total_retard + (vol.ttot - vol.etot), count + 1, 0, nb_creneaux_rates.(heure))
+  ) vols;
+  
+  (* Créez une hashtable et ajoutez-y les vols *)
+  let hashtbl_parkings = Parking.ParkingHashtbl.create (List.length vols) in
+  Parking.remplir_hashtbl vols hashtbl_parkings;
+  
+  (* Appelez Parking.calcul_conflit_parking avec la hashtable *)
+  Parking.calcul_conflit_parking vols hashtbl_parkings;
+  
+  (* Mettez à jour les conflits de parking dans le tableau heures *)
+  Parking.ParkingHashtbl.iter (fun _ vols_parking ->
     List.iter (fun (vol : Vol.t) ->
       let heure = vol.etot / 3600 in
-      let (total_retard, count, _, _) = heures.(heure) in
-      heures.(heure) <- (total_retard + (vol.ttot - vol.etot), count + 1, nb_conflits_parking.(heure), nb_creneaux_rates.(heure))
-    ) vols;
-    let fichier = open_out nom_fichier in
-    fprintf fichier "Heure,NB_vols,Retard moyen (minutes),Nb_conflits_parking,Nb_creneaux_rates\n";
-    Array.iteri (fun heure (total_retard, count, conflits_parking, creneaux_rates) ->
-      if count > 0 then
-        fprintf fichier "%02d,%d,%.2f,%d,%d\n" heure nb_vols.(heure) (float_of_int total_retard /. float_of_int count /. 60.0) conflits_parking creneaux_rates
-      else
-        fprintf fichier "%02d,%d,Aucun vol,%d,%d\n" heure nb_vols.(heure) conflits_parking creneaux_rates
-    ) heures;
-    close_out fichier
+      let (total_retard, count, conflits_parking, creneaux_rates) = heures.(heure) in
+      heures.(heure) <- (total_retard, count, conflits_parking + 1, creneaux_rates)
+    ) vols_parking
+  ) hashtbl_parkings;
+  
+  let fichier = open_out nom_fichier in
+  Printf.fprintf fichier "Heure,NB_vols,Retard moyen (minutes),Nb_conflits_parking,Nb_creneaux_rates\n";
+  Array.iteri (fun heure (total_retard, count, conflits_parking, creneaux_rates) ->
+    if count > 0 then
+      Printf.fprintf fichier "%02d,%d,%.2f,%d,%d\n" heure nb_vols.(heure) (float_of_int total_retard /. float_of_int count /. 60.0) conflits_parking creneaux_rates
+    else
+      Printf.fprintf fichier "%02d,%d,Aucun vol,%d,%d\n" heure nb_vols.(heure) conflits_parking creneaux_rates
+  ) heures;
+  close_out fichier
