eb65704171e19fcb0c9a95ad15b024282ad428a1
Paamadoud <diengpa@ept.sn>
2025-01-05 15:12:49 +0100

    lecture parametres

--- parametre.ml
+++ parametre.ml
@@ -7,24 +7,22 @@ module Params = struct
   }
 
   (* Fonction pour créer une configuration *)
-  let create_config parking_time type_separation = { parking_time ; type_separation }
+  let create_config parking_time type_separation = { parking_time; type_separation }
 end
 
-
-(* Programme principal à mettre dans le main normalement*)
+(* Programme principal à mettre dans le main normalement *)
 let () =
   (* Lire les paramètres depuis l'entrée utilisateur *)
-  print_string "Entrez le temps d'occupation d'un parking en minutes (int) : ";
+  print_string "Entrez le temps d'occupation d'un parking (int) : ";
   let park_time = read_int () in
 
-  print_string "Entrez le temps de séparation ("M", "L") | ("H", "L") | ("H", "M") : ";
-  let sep_1= read_int () in
-  print_string "Entrez le temps de séparation ("H", "H"): ";
+  print_string "Entrez le temps de separation ("M", "L") | ("H", "L") | ("H", "M"): ";
+  let sep_1 = read_int () in
+  print_string "Entrez le temps de separation ("H", "H"): ";
   let sep_2 = read_int () in
-  print_string "Entrez le temps de séparation pour le reste  : ";
+  print_string "Entrez le temps de separation pour le reste : ";
   let sep_3 = read_int () in
 
   (* Créer la structure de configuration *)
-  let config = Params.create_config park_time (sep_1, sep_2, sep_3) in
-
- 
+  let config = Params.create_config park_time (sep_1, sep_2, sep_3) 
+  
